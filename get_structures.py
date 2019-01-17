91a2250c77b1e039a78e4171ac4e2e5a88a26a5f
Mitchell Koerner <mfkoerner@gmail.com>
2019-01-16 18:16:31 -0800

    get_structures: added

---
+++ get_structures.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 18:05:05 2019
+
+@author: mitchell koerner
+@email:  mfkoerner@gmail.com
+"""
+
+
+import qmpy         #quantum materials database interface
+import numpy
+import os
+import sys
+
+from qmpy import Structure
