18b82e104b476c7118d8416a1f5ef4a7e118a454
Mitchell Koerner <mfkoerner@gmail.com>
2019-01-18 14:01:46 -0800

    automatic update

--- get_structures.py
+++ get_structures.py
@@ -10,6 +10,7 @@ Created on Wed Jan 16 18:05:05 2019
 
 import pymatgen
 from pymatgen.analysis.bond_valence import BVAnalyzer
+from pymatgen import structure
 import qmpy         #quantum materials database interface
 import numpy
 import os
