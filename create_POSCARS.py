e06ce15824dfa85ab5216e914e26790b35dc7ca4
Mitchell Koerner <mfkoerner@gmail.com>
2019-01-30 15:08:04 -0800

    automatic update

--- create_POSCARS.py
+++ create_POSCARS.py
@@ -10,9 +10,15 @@ Created on Wed Jan 30 14:31:35 2019
 import get_structures as gs
 from filter_structures import final_list
 import os
+from qmpy import io
 
 bonuses = [gs.InversePerovskiteBonuses(i) for i in final_list]
 
+# make base directories
+# for bonus in bonuses:
+#     os.mkdir('/home/oqmd/pod/mkoerner/inpertop_data/icsd/I{}'.format(bonus.idstr))
+
+# create POSCAR files
 for bonus in bonuses:
-    os.mkdir('/home/oqmd/pod/mkoerner/inpertop_data/icsd/I{}'.format(bonus.idstr))
-    
+    io.write(bonus.structure, 
+        filename='/home/oqmd/pod/mkoerner/inpertop_data/icsd/I{}/POSCAR'.format(bonus.idstr))
