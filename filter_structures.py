76cffb372cafc722e59211109a0b144fa3806f8a
Mitchell Koerner <mfkoerner@gmail.com>
2019-01-30 14:33:56 -0800

    automatic update

--- filter_structures.py
+++ filter_structures.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 30 14:31:35 2019
+Created on Wed Jan 30 14:33:43 2019
 
 @author: mitchell koerner
 @email:  mfkoerner@gmail.com
 """
-
-import get_structures as gs
 
