300724c6a641a0d8e77ca76d982fdeeeb920f6fd
Mitchell Koerner <mfkoerner@gmail.com>
2019-02-13 13:06:46 -0800

    automatic update

---
+++ incar_tool.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 13 13:03:53 2019
+
+@author: mitchell koerner
+@email:  mfkoerner@gmail.com
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
