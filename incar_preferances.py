39d8cddcc8bcfcbfa2856131cde796b5f7db2b19
Mitchell Koerner <mfkoerner@gmail.com>
2019-02-13 13:29:25 -0800

    automatic update

---
+++ incar_preferances.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 13 13:25:11 2019
+
+@author: mitchell koerner
+@email:  mfkoerner@gmail.com
+"""
+
+
+
+
+from pymatgen.io.vasp import Incar
+
+
+
+
+
+
+
+
