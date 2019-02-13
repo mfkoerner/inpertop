62dd772c6c31d65c48517c7a47bd93e8f11682c6
Mitchell Koerner <mfkoerner@gmail.com>
2019-02-13 13:14:48 -0800

    automatic update

--- incar_tool.py
+++ incar_tool.py
@@ -11,6 +11,24 @@ Created on Wed Feb 13 13:03:53 2019
 
 
 
+class Incar():
+    """
+    Docstring
+    """
+    def __init__(self, system = None, read_fp = None):
+        self.data = {}
+        if not read_fp is None:
+            self.read(read_fp)
+        if system is None:
+            self.system = 'unknown'
+        else:
+            self.system = system
+
+    def read(self, filepath = 'INCAR', set_system = False):
+        """
+        reads INCAR to data
+        """
+        with open(filepath, 'r') as f:
 
 
 
